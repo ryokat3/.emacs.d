e60372e5768d7e41ef62f50e0b8dafa44bec4329
U-ERICSSON\nrjryka <nrjryka@JP00086284.ericsson.se>
2016-08-25 13:42:07 +0900

    Changes to be committed: 	modified:   init.el

--- init.el
+++ init.el
@@ -71,7 +71,7 @@
 ;;;
 ;;; Japanese Fonts
 ;;;
-(if windows-p
+(if (and window-system windows-p)
     (progn
       (setq ms-gothic-string (encode-coding-string "ＭＳ ゴシック" 'sjis))
       (set-default-font (concat ms-gothic-string " 12"))
@@ -216,45 +216,42 @@
 ;;;
 ;;; Tabber.el
 ;;;
-(require 'tabbar)
-(tabbar-mode)
-(global-set-key "\M-]" 'tabbar-forward)  ; 次のタブ
-(global-set-key "\M-[" 'tabbar-backward) ; 前のタブ
-;; タブ上でマウスホイールを使わない
-(tabbar-mwheel-mode nil)
-;; グループを使わない
-(setq tabbar-buffer-groups-function nil)
-;; 左側のボタンを消す
-(dolist (btn '(tabbar-buffer-home-button
-               tabbar-scroll-left-button
-               tabbar-scroll-right-button))
-  (set btn (cons (cons "" nil)
-                 (cons "" nil))))
-;; 色設定
-;;
-(set-face-attribute ; バー自体の色
-  'tabbar-default nil
-   :background "white"
-   :family "Inconsolata"
-   :height 0.75)  ; same font size with buffer if height is 1.0
-(set-face-attribute ; アクティブなタブ
-  'tabbar-selected nil
-   :background "black"
-   :foreground "white"
-   :weight 'bold
-   :box nil)
-(set-face-attribute ; 非アクティブなタブ
-  'tabbar-unselected nil
-   :background "white"
-   :foreground "black"
-   :box nil)
-
-;;;
-;;; GPG
-;;;
-(require 'epa-file)
-(epa-file-enable)
-(setq epa-file-cache-passphrase-for-symmetric-encryption t)
+(defun tabbar-init()
+  (progn
+    (require 'tabbar)
+    (tabbar-mode)
+    (global-set-key "\M-]" 'tabbar-forward)  ; 次のタブ
+    (global-set-key "\M-[" 'tabbar-backward) ; 前のタブ
+    ;; タブ上でマウスホイールを使わない
+    (tabbar-mwheel-mode nil)
+    ;; グループを使わない
+    (setq tabbar-buffer-groups-function nil)
+    ;; 左側のボタンを消す
+    (dolist (btn '(tabbar-buffer-home-button
+		   tabbar-scroll-left-button
+		   tabbar-scroll-right-button))
+      (set btn (cons (cons "" nil)
+		     (cons "" nil))))
+    ;; 色設定
+    ;;
+    (set-face-attribute ; バー自体の色
+     'tabbar-default nil
+     :background "white"
+     :family "Inconsolata"
+     :height 0.75)  ; same font size with buffer if height is 1.0
+    (set-face-attribute ; アクティブなタブ
+     'tabbar-selected nil
+     :background "black"
+     :foreground "white"
+     :weight 'bold
+     :box nil)
+    (set-face-attribute ; 非アクティブなタブ
+     'tabbar-unselected nil
+     :background "white"
+     :foreground "black"
+     :box nil)
+    ))
+(if window-system tabber-init)
 
 ;;;
 ;;; Theme
