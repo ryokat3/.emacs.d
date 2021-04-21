be30a834ac8967042ec3956a8aca2997120c184c
Ryoji Kato <ryoji.kato@ericsson.com>
2021-04-22 00:25:13 +0900

    tr-ime

--- init.el
+++ init.el
@@ -9,27 +9,23 @@
 ;;;
 ;;; Windows 7 - 10 ::
 ;;;
-;;; ~/AppData/Roaming/.emacs.d/init.el
+;;; 1. Update ~/AppData/Roaming/.emacs.d/init.el
 ;;;
-;;; (setenv "HOME" "C:/Users/XXXX")
-;;; (setq user-emacs-directory "~/.emacs.d")
-;;; (load-file (expand-file-name "init.el" user-emacs-directory))
+;;;   (setenv "HOME" "C:/Users/XXXX")
+;;;   (setq user-emacs-directory "~/.emacs.d")
+;;;   (load-file (expand-file-name "init.el" user-emacs-directory))
 ;;;
+;;;
+;;; 2. Install packages:
+;;;
+;;;   - tr-ime
+;;;   - w32-ime
 
 ;;;
 ;;; Registry File alist
 ;;;
 ;;;  ( hostname . registry-file )
 ;;;
-
-; (package-initialize)
-
-;;;
-;;; Spacemacs
-;;;
-;(setq spacemacs-start-directory "~/.emacs.d/spacemacs/")
-;(load-file (concat spacemacs-start-directory "init.el"))
-
 
 ;; Surpress the warning 'Package cl is deprecated' in emacs 27.x
 ;;
@@ -91,12 +87,6 @@
                           ("melpa" . "https://melpa.org/packages/")))
 
 ;;;
-;;; tr-ime
-;;;
-
-
-
-;;;
 ;;; System Type
 (setq windows-p (eq system-type 'windows-nt))
 (setq linux-p (eq system-type 'gnu/linux))
@@ -125,55 +115,56 @@
 
 ;; 日本語入力のための設定
 (when windows-p
-  (tr-ime-advanced-install)
-  (setq default-input-method "W32-IME")
   (set-keyboard-coding-system 'cp932)
   (prefer-coding-system 'utf-8-dos)
   (set-file-name-coding-system 'cp932)
   (setq default-process-coding-system '(cp932 . cp932))
-
-  (when (fboundp 'w32-ime-initialize)
-    (w32-ime-initialize)
-
-    ;; IME 制御（yes/no などの入力の時に IME を off にする）
-    (wrap-function-to-control-ime 'universal-argument t nil)
-    (wrap-function-to-control-ime 'read-string nil nil)
-    (wrap-function-to-control-ime 'read-char nil nil)
-    (wrap-function-to-control-ime 'read-from-minibuffer nil nil)
-    (wrap-function-to-control-ime 'y-or-n-p nil nil)
-    (wrap-function-to-control-ime 'yes-or-no-p nil nil)
-    (wrap-function-to-control-ime 'map-y-or-n-p nil nil)
-
-
-    ;; (set-language-environment "UTF-8") ;; UTF-8 でも問題ないので適宜コメントアウトしてください
-    (setq default-input-method "W32-IME")
-    (setq-default w32-ime-mode-line-state-indicator "[--]")
-    (setq w32-ime-mode-line-state-indicator-list '("[--]" "[あ]" "[--]"))
-
-    ;; 日本語入力時にカーソルの色を変える設定 (色は適宜変えてください)
-    (add-hook 'w32-ime-on-hook '(lambda () (set-cursor-color "coral4")))
-    (add-hook 'w32-ime-off-hook '(lambda () (set-cursor-color "yellow")))
-
-    ;; ミニバッファに移動した際は最初に日本語入力が無効な状態にする
-    (add-hook 'minibuffer-setup-hook 'deactivate-input-method)
-
-    ;; isearch に移行した際に日本語入力を無効にする
-    (add-hook 'isearch-mode-hook
-	      '(lambda ()
-		 (deactivate-input-method)
-		 (setq w32-ime-composition-window (minibuffer-window))))
-    (add-hook 'isearch-mode-end-hook
-	      '(lambda () (setq w32-ime-composition-window nil)))
-
-    ;; helm 使用中に日本語入力を無効にする
-    (advice-add 'helm :around
-		'(lambda (orig-fun &rest args)
-		   (let ((select-window-functions nil)
-			 (w32-ime-composition-window (minibuffer-window)))
-		     (deactivate-input-method)
-		     (apply orig-fun args))))
-    )
   )
+
+;; tr-ime + w32-ime 設定
+(when (and (require 'tr-ime nil t) (require 'w32-ime nil t))
+  (tr-ime-advanced-install)
+  (setq default-input-method "W32-IME")
+  (w32-ime-initialize)
+
+  ;; IME 制御（yes/no などの入力の時に IME を off にする）
+  (wrap-function-to-control-ime 'universal-argument t nil)
+  (wrap-function-to-control-ime 'read-string nil nil)
+  (wrap-function-to-control-ime 'read-char nil nil)
+  (wrap-function-to-control-ime 'read-from-minibuffer nil nil)
+  (wrap-function-to-control-ime 'y-or-n-p nil nil)
+  (wrap-function-to-control-ime 'yes-or-no-p nil nil)
+  (wrap-function-to-control-ime 'map-y-or-n-p nil nil)
+
+  ;; (set-language-environment "UTF-8") ;; UTF-8 でも問題ないので適宜コメントアウトしてください
+  (setq default-input-method "W32-IME")
+  (setq-default w32-ime-mode-line-state-indicator "[--]")
+  (setq w32-ime-mode-line-state-indicator-list '("[--]" "[あ]" "[--]"))
+
+  ;; 日本語入力時にカーソルの色を変える設定 (色は適宜変えてください)
+  (add-hook 'w32-ime-on-hook '(lambda () (set-cursor-color "coral4")))
+  (add-hook 'w32-ime-off-hook '(lambda () (set-cursor-color "yellow")))
+
+  ;; ミニバッファに移動した際は最初に日本語入力が無効な状態にする
+  (add-hook 'minibuffer-setup-hook 'deactivate-input-method)
+
+  ;; isearch に移行した際に日本語入力を無効にする
+  (add-hook 'isearch-mode-hook
+	    '(lambda ()
+	       (deactivate-input-method)
+	       (setq w32-ime-composition-window (minibuffer-window))))
+  (add-hook 'isearch-mode-end-hook
+	    '(lambda () (setq w32-ime-composition-window nil)))
+
+  ;; helm 使用中に日本語入力を無効にする
+  (advice-add 'helm :around
+	      '(lambda (orig-fun &rest args)
+		 (let ((select-window-functions nil)
+		       (w32-ime-composition-window (minibuffer-window)))
+		   (deactivate-input-method)
+		   (apply orig-fun args))))
+  )
+
 
 ;;;
 ;;; Tool Bar
